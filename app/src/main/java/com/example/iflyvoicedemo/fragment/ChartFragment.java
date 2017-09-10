99fac951662ece9c4f544712719dc51b1da96ced
wumin@sunland <fusu1435@163.com>
2017-09-10 10:47:48 +0800

    Optimisum chart.

--- app/src/main/java/com/example/iflyvoicedemo/fragment/ChartFragment.java
+++ app/src/main/java/com/example/iflyvoicedemo/fragment/ChartFragment.java
@@ -3,6 +3,8 @@ package com.example.iflyvoicedemo.fragment;
 
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -16,6 +18,7 @@ import com.example.iflyvoicedemo.R;
 import com.example.iflyvoicedemo.bean.VoiceResult;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.LimitLine;
 import com.github.mikephil.charting.components.XAxis;
@@ -26,6 +29,7 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
@@ -109,49 +113,48 @@ public class ChartFragment extends Fragment {
     }
 
     private void initLineChart() {
-        lineChartErr.setDrawGridBackground(true);
-        lineChartErr.setDescription(null);
+        Description desc = lineChartErr.getDescription();
+        desc.setText("准确率统计");
+        desc.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorBlack));
+        desc.setYOffset(-12);
         lineChartErr.setNoDataText(getString(R.string.speak_result_null));
         lineChartErr.setTouchEnabled(true);
         lineChartErr.setDragEnabled(true);
         lineChartErr.setScaleEnabled(true);
-
+        lineChartErr.setDrawGridBackground(true);
         lineChartErr.setPinchZoom(true);
-        lineChartErr.setBackgroundColor(Color.WHITE);
-
-//        MyMarkerView mv = new MyMarkerView(this, R.layout.custom_marker_view);
-//        mv.setChartView(accuracyChart); // For bounds control
-//        accuracyChart.setMarker(mv); // Set the marker to the chart
-        LimitLine llXAxis = new LimitLine(10f, "Index 10");
-        llXAxis.setLineWidth(4f);
-        llXAxis.enableDashedLine(10f, 10f, 1f);
-        llXAxis.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
-        llXAxis.setTextSize(10f);
 
         XAxis xAxis = lineChartErr.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setEnabled(true);
         xAxis.setDrawAxisLine(true);
-        xAxis.setLabelCount(0);
+        xAxis.setLabelCount(1);
         xAxis.setAvoidFirstLastClipping(true);
+        xAxis.setAxisMinimum(0f);
+        xAxis.setSpaceMin(1f);
+        xAxis.setSpaceMax(1f);
+        xAxis.setDrawLabels(false);
 
         Typeface tf = Typeface.createFromAsset(getActivity().getAssets(), "OpenSans-Regular.ttf");
-        LimitLine bestLine = new LimitLine(0.9f, "优");
-        bestLine.setLineWidth(3f);
+        LimitLine bestLine = new LimitLine(90f, "优");
+        bestLine.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorRed));
+        bestLine.setLineWidth(2f);
         bestLine.enableDashedLine(10f, 10f, 0f);
         bestLine.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
         bestLine.setTextSize(10f);
         bestLine.setTypeface(tf);
 
-        LimitLine goodLine = new LimitLine(0.5f, "平均值");
-        goodLine.setLineWidth(3f);
+        LimitLine goodLine = new LimitLine(50f, "平均值");
+        goodLine.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorRed));
+        goodLine.setLineWidth(2f);
         goodLine.enableDashedLine(10f, 10f, 0f);
         goodLine.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
         goodLine.setTextSize(10f);
         goodLine.setTypeface(tf);
 
-        LimitLine badLine = new LimitLine(0.1f, "差");
-        badLine.setLineWidth(3f);
+        LimitLine badLine = new LimitLine(10f, "差");
+        badLine.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorRed));
+        badLine.setLineWidth(2f);
         badLine.enableDashedLine(10f, 10f, 0f);
         badLine.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
         badLine.setTextSize(10f);
@@ -162,11 +165,12 @@ public class ChartFragment extends Fragment {
         leftAxis.addLimitLine(bestLine);
         leftAxis.addLimitLine(goodLine);
         leftAxis.addLimitLine(badLine);
-        leftAxis.setAxisMaximum(1.0f);
+        leftAxis.setAxisMaximum(100f);
         leftAxis.setAxisMinimum(0.0f);
         //leftAxis.setYOffset(20f);
         leftAxis.enableGridDashedLine(10f, 10f, 0f);
         leftAxis.setDrawZeroLine(true);
+        leftAxis.setValueFormatter(new PercentFormatter());
 
         // limit lines are drawn behind data (and not on top)
         leftAxis.setDrawLimitLinesBehindData(true);
@@ -174,13 +178,16 @@ public class ChartFragment extends Fragment {
         lineChartErr.animateX(2500);
 
         Legend offlineLegend = lineChartErr.getLegend();
-
+        offlineLegend.setFormSize(5);
         offlineLegend.setForm(Legend.LegendForm.LINE);
         initLineData();
     }
 
     private void initBarChart() {
-        chartErr.setDescription(null);
+        Description desc = chartErr.getDescription();
+        desc.setText("错误数量统计");
+        desc.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorBlack));
+        desc.setYOffset(-12);
         chartErr.setNoDataText(getString(R.string.speak_result_null));
         chartErr.setNoDataTextColor(Color.BLACK);
         // if more than 60 entries are displayed in the chart, no values will be
@@ -188,17 +195,27 @@ public class ChartFragment extends Fragment {
         chartErr.setMaxVisibleValueCount(60);
 
         // scaling can now only be done on x- and y-axis separately
-        chartErr.setPinchZoom(false);
+        chartErr.setPinchZoom(true);
 
         chartErr.setDrawBarShadow(false);
-        chartErr.setDrawGridBackground(false);
+        chartErr.setDrawGridBackground(true);
+        chartErr.setDrawValueAboveBar(true);
 
         XAxis xAxis = chartErr.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setDrawGridLines(false);
+        xAxis.setSpaceMin(1.0f);
+        xAxis.setSpaceMax(1.0f);
+        xAxis.setAxisMinimum(0f);
+        xAxis.setDrawLabels(false);
 
-        chartErr.getAxisLeft().setDrawGridLines(false);
-
+        YAxis yAxis = chartErr.getAxisLeft();
+        yAxis.setDrawGridLines(true);
+        yAxis.enableGridDashedLine(10f, 10f, 0f);
+        yAxis.setAxisMinimum(0f);
+        yAxis.setSpaceMin(1f);
+        yAxis.setSpaceMax(10f);
+        chartErr.getAxisRight().setEnabled(false);
         // add a nice and smooth animation
         chartErr.animateY(2500);
 
@@ -221,16 +238,18 @@ public class ChartFragment extends Fragment {
             offlineSet.setDrawIcons(true);
             offlineSet.setColor(ContextCompat.getColor(getActivity(), R.color.colorMediumAquamaine));
             offlineSet.setCircleColor(ContextCompat.getColor(getActivity(), R.color.colorMediumAquamaine));
-            offlineSet.setLineWidth(1f);
+            offlineSet.setLineWidth(2f);
             offlineSet.setCircleRadius(3f);
             offlineSet.setDrawCircleHole(true);
-            offlineSet.setValueTextSize(9f);
+            offlineSet.setValueTextSize(12f);
+            offlineSet.setValueTextColor(ContextCompat.getColor(getActivity(), R.color.colorforestGreen));
             offlineSet.setDrawFilled(true);
             offlineSet.setFormLineWidth(1f);
             offlineSet.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
             offlineSet.setFormSize(15.f);
-            offlineSet.setFillDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.shape_fade_blue));
+            offlineSet.setFillColor(ContextCompat.getColor(getActivity(), R.color.colorMediumAquamaine));
 
+            offlineSet.setValueFormatter(new PercentFormatter());
             ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
             dataSets.add(offlineSet);
 
@@ -249,14 +268,16 @@ public class ChartFragment extends Fragment {
             chartErr.getData().notifyDataChanged();
             chartErr.notifyDataSetChanged();
         } else {
-            barSet = new BarDataSet(yVals, "Data Set");
+            barSet = new BarDataSet(yVals, "准确率");
             barSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
-            barSet.setDrawValues(false);
+            barSet.setDrawValues(true);
+            barSet.setFormLineWidth(4);
 
             ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
             dataSets.add(barSet);
 
             BarData data = new BarData(dataSets);
+            data.setBarWidth(0.5f);
             chartErr.setData(data);
             chartErr.setFitBars(true);
         }
@@ -267,20 +288,25 @@ public class ChartFragment extends Fragment {
     private void initData() {
         yVals = new ArrayList<>();
         yOfflineVals = new ArrayList<>();
+
         Realm realm = null;
         try {
             realm = Realm.getDefaultInstance();
             RealmResults<VoiceResult> results = realm.where(VoiceResult.class)
                                                             .equalTo("type", mType).findAll();
             if (results != null) {
-                for (int i = 0; i < results.size(); i++) {
+                for (int i = 1; i <= results.size(); i++) {
                     VoiceResult result = results.get(i);
                     if (result != null) {
                         yVals.add(new BarEntry(i, result.getErrs()));
-                        yOfflineVals.add(new Entry(i, result.getAccuracy()));
+                        yOfflineVals.add(new Entry(i, result.getAccuracy()*100));
                         Log.d(TAG, result.toString());
                     }
                 }
+            }
+
+            if (yOfflineVals.size() == 0) {
+                yOfflineVals.add(new Entry(0, 0));
             }
 
         } catch (Exception e) {
